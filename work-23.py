3fcf2df2f900ee824146b031cce5263b68833f8e
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:27:53 +0300

    Add files via upload

---
+++ work-23.py
@@ -0,0 +1,90 @@
+#1-misol
+#k=int(input("k="))
+#n=int(input("n="))
+#for i in range(n):
+#    print(k)
+
+#2-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#s=0
+#for i in range(a, b+1):
+#    s+=1
+#    print(i)
+#print("s=", s)
+
+#3-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#s=0
+#for i in range(b-1, a,-1):
+#    s+=1
+#    print(i)
+#print("s=", s)
+
+#4-misol
+#kompt=int(input("kompt="))
+#for i in range(1, 11):
+#    print("komptni narxi ", kompt, "so'm",i,"kg olsang",i*kompt,"so'm bo'ladi maxluq")
+
+#5-misol  
+#kompt=int(input("kompt="))
+#for i in range (1, 11):
+ #   print(i/10, "kg narxi=", kompt*i/10 )
+#6-misol
+#konfet=int(input("Konfet="))
+#for i in range (12, 22, 2):
+    #print(i/10, "kg narxi=", konfet*i/10)
+#7-misol
+#a=int(input("a="))
+#b=int(input("b="))  
+#s=0
+#for i in range(a, b+1):
+#    s+=i
+ #   print(i)
+#print(s)#1-misol
+#k=int(input("k="))
+#n=int(input("n="))
+#for i in range(n):
+#    print(k)
+
+#2-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#s=0
+#for i in range(a, b+1):
+#    s+=1
+#    print(i)
+#print("s=", s)
+
+#3-misol
+#a=int(input("a="))
+#b=int(input("b="))
+#s=0
+#for i in range(b-1, a,-1):
+#    s+=1
+#    print(i)
+#print("s=", s)
+
+#4-misol
+#kompt=int(input("kompt="))
+#for i in range(1, 11):
+#    print("komptni narxi ", kompt, "so'm",i,"kg olsang",i*kompt,"so'm bo'ladi maxluq")
+
+#5-misol  
+#kompt=int(input("kompt="))
+#for i in range (1, 11):
+ #   print(i/10, "kg narxi=", kompt*i/10 )
+#6-misol
+#konfet=int(input("Konfet="))
+#for i in range (12, 22, 2):
+    #print(i/10, "kg narxi=", konfet*i/10)
+#7-misol
+#a=int(input("a="))
+#b=int(input("b="))  
+#s=0
+#for i in range(a, b+1):
+#    s+=i
+ #   print(i)
+#print(s)
+
